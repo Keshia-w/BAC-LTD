7df28b48825133de59ef56996d68d974de98a797
Keshia-w <keshia.w196@gmail.com>
2025-01-27 14:30:30 +1300

    Dependencies installed, vite information cleared

--- bac/src/App.tsx
+++ bac/src/App.tsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react'
 import axios from "axios"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Box,
+
+} from "@chakra-ui/react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api")
@@ -17,28 +16,9 @@ function App() {
   }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box>
+      
+    </Box>
   )
 }
 
