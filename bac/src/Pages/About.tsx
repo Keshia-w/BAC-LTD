1b6c6f32a5abe677ce2f9b0b3ac89657b3055c0f
Keshia-w <keshia.w196@gmail.com>
2025-01-29 10:18:45 +1300

    Header text added to pages

--- bac/src/Pages/About.tsx
+++ bac/src/Pages/About.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import {
     Box,
+    Text,
 } from '@chakra-ui/react'
 
 function About () {
     return(
         <Box>
-            
+            <Box         
+                w={{ base: "90%", sm: "85%", md: "75%", lg: "60%", xl: "50%" }}
+                mx="auto"
+                margin={"auto"}
+                mt={{ base: 10, md: 5 }}
+                position={"relative"}>
+
+                <Text 
+                fontWeight="semibold" 
+                fontSize="28px" 
+                letterSpacing={"0.1rem"}
+                textAlign="center">About us</Text>
+            </Box>
         </Box>
     )
 }
