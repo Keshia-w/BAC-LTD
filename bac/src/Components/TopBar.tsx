7e5eb05b7346f91a2aaa4b47f4f5c106f120da15
Keshia-w <keshia.w196@gmail.com>
2025-01-29 10:16:30 +1300

    Logo routes to home page

--- bac/src/Components/TopBar.tsx
+++ bac/src/Components/TopBar.tsx
@@ -14,9 +14,11 @@ function TopBar() {
 
     return(
         <Box width="100%" height="80px" backgroundColor="#151B27">
-            <Box display="flex" gap="45px">
-                <Image src="./src/assets/logo.png" width="180px" height="80px" marginLeft="15px"></Image>
-                
+            <Box>
+                <Link to="/">
+                    <Image src="./src/assets/logo.png" width="180px" height="80px" marginLeft="15px"></Image>
+                </Link>
+
                 <Menu>
                     <MenuItem as={Link} to="/about">About Us</MenuItem>
                     <MenuItem as={Link} to="/gallery">Gallery</MenuItem>
